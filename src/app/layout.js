b659b1fc071842c95be7d1210b572b96ff125bcd
Stiwyy <andrinrenggli@outlook.com>
2026-02-02 19:36:11 +0100

    add website icon

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,10 @@ import "./globals.css";
 export const metadata = {
   title: "Brain Trainer",
   description: "Train your Brain with easy exercises",
+  icons: {
+    icon: "https://img.icons8.com/fluency/96/brain.png",
+    apple: "https://img.icons8.com/fluency/96/brain.png",
+  },
 };
 
 export default function RootLayout({ children }) {
